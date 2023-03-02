1ef028f5e26d66fe759fa997dece3ba002c74732
Ani <ani.mkrtchyan1982@gmail.com>
2023-03-02 20:35:40 +0400

    Some change.

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,16 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE= 18;
+
+    private  int age;
+    private int phoneNumber;
+
+    public void callToFriend() {
+        System.out.println("Hello");  //some code
+    }
+
+    public int callByNumber(int number) {
+      return phoneNumber;  //some code
+    }
+}
--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int morrow = 0;
+        int tomorrow = 0;
         int money = 10;
         int hundred = 100;
         int back = 1000;
@@ -12,3 +12,8 @@ public class JavaVariableNames {
         int BAD_EXAMPLE_OF_VAR_NAME = 1000000;
     }
 }
+
+
+
+
+
